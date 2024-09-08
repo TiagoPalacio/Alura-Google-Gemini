29d8a6c6ac0c3a77f63ab03e6d9137f14e768033
Tiago Palácio <163942587+TiagoPalacio@users.noreply.github.com>
2024-09-08 11:23:13 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log(dados);
+
+let section = document.getElementById("resultados-pesquisa");
+console.log(section);
+
+section.innerHTML = <div class="item-resultado">
+                <h2>Celeste</h2>
+                <p class="descricao-meta">Celeste é um jogo indie de plataforma 2D que combina desafios precisos com uma narrativa tocante sobre superação pessoal. Desenvolvido por Matt Thorson e Noel Berry, Celeste leva os jogadores em uma jornada pela Montanha Celeste, onde a protagonista, Madeline, enfrenta seus medos e inseguranças. Com uma jogabilidade fluida e uma trilha sonora memorável, Celeste oferece uma experiência emocional e gratificante.</p>
+                <a href="https://www.celestegame.com" target="_blank">Sobre</a>
+            </div>
---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+      titulo: "Dandara",
+      descricao: "Dandara é um jogo indie brasileiro de plataforma e metroidvania 2D que cativou jogadores com sua estética visual única, jogabilidade inovadora e narrativa envolvente. Desenvolvido pelo estúdio Long Hat House e publicado pela Raw Fury, o jogo nos transporta para um universo bizarro onde a gravidade não é uma constante e a realidade se distorce.",
+      link: "https://pt.wikipedia.org/wiki/Dandara_(jogo_eletrônico)"
+    },
+    {
+      titulo: "Celeste",
+      descricao: "Celeste é um jogo indie de plataforma 2D que combina desafios precisos com uma narrativa tocante sobre superação pessoal. Desenvolvido por Matt Thorson e Noel Berry, Celeste leva os jogadores em uma jornada pela Montanha Celeste, onde a protagonista, Madeline, enfrenta seus medos e inseguranças. Com uma jogabilidade fluida e uma trilha sonora memorável, Celeste oferece uma experiência emocional e gratificante.",
+      link: "https://www.celestegame.com"
+    },
+    {
+      titulo: "Horizon Chase Turbo",
+      descricao: "Horizon Chase Turbo é um jogo de corrida arcade que evoca a nostalgia dos clássicos dos anos 80 e 90. Desenvolvido pela Aquiris Game Studio, o jogo oferece uma experiência de corrida rápida e divertida, com visuais vibrantes e uma trilha sonora inspirada nas clássicas trilhas de jogos de corrida. Prepare-se para acelerar em pistas desafiadoras e vivenciar a emoção das corridas retrô.",
+      link: "https://www.horizonchase.com"
+    }
+  ];
